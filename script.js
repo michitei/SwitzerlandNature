4236bb9026babb68ad15041933f57392c73eb483
michitei <95420458+michitei@users.noreply.github.com>
2023-11-15 12:15:25 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,41 @@
+let option = ['One Day Tour','3 Days Tour', '7 Days Tour'];
+if (option = [0]) {
+   console.log('One Day Tour');
+}
+
+else if (option = [1]) {
+    console.log('3 Days Tour');
+}
+
+else if (option = [2]) {
+    console.log('7 Days Tour');
+}
+
+const websiteName = "Switzerland Nature";
+const websiteURL = "https://www.switzerlandnature.com";
+const websiteDescription = "Explore the breathtaking natural beauty of Switzerland.";
+
+const destinations = [
+  "Swiss Alps",
+  "Lake Geneva",
+  "Zermatt and the Matterhorn",
+  "Jungfrau Region",
+  "Swiss National Park"
+];
+
+const attractionImages = [
+  "alps.jpg",
+  "lake-geneva.jpg",
+  "matterhorn.jpg",
+  "jungfrau.jpg",
+  "swiss-national-park.jpg"
+];
+
+const attractionDescriptions = [
+  "Experience the majestic Swiss Alps with their snow-capped peaks.",
+  "Visit the stunning shores of Lake Geneva and enjoy its crystal-clear waters.",
+  "Discover the iconic Matterhorn mountain in the charming town of Zermatt.",
+  "Explore the breathtaking Jungfrau region with its picturesque landscapes.",
+  "Immerse yourself in the natural wonders of the Swiss National Park."
+];
+
